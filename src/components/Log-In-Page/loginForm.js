9ac1b83c6aecdf6113d3bb3afc23e03cb4f6b7be
Jennifer Colna <jennifer.colna@gmail.com>
2018-08-08 09:58:10 -0500

    login form created

---
+++ src/components/Log-In-Page/loginForm.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {reduxForm, Field, focus} from 'redux-form';
+import {login} from '../../actions/Log-In-Actions/loginAction';
+import Input from '../input';
+//need validators
+
+class LoginForm extends React.Component {
+  render(){
+    let error;
+    if(this.props.error){
+      error = (
+        <div className="form-error" aria-live="polite">
+          {this.props.error}
+        </div>
+      );
+    }
+    return (   
+      <form
+        onSubmit={this.props.handleSubmit(values => this.props.dispatch(login(values.username,values.password)))}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <Field 
+            component={Input}
+            type="text" 
+            name="username"
+            id="username"
+            /* validate=?? */>
+          </Field>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <Field 
+            component={Input}
+            type="password" 
+            name="password"
+            id="password"
+            /* validate=?? */>
+          </Field>
+        </div>
+        <div className="button-container">
+          <button disabled={this.props.pristine||this.props.submitting}>
+            Log-In
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'login',
+  onSubmitFail: (errors, dispatch)=> dispatch(focus('login', 'username'))
+})(LoginForm);
